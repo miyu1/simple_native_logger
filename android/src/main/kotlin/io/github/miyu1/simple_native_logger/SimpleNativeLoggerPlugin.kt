28a38c6cb1c75f6b20434d0c668f60b1aa10da71
Miyako Komooka <miyu1.dev@gmail.com>
2024-10-06 21:40:32 +0900

    import android.util.Log

--- android/src/main/kotlin/io/github/miyu1/simple_native_logger/SimpleNativeLoggerPlugin.kt
+++ android/src/main/kotlin/io/github/miyu1/simple_native_logger/SimpleNativeLoggerPlugin.kt
@@ -1,6 +1,7 @@
 package io.github.miyu1.simple_native_logger
 
 import androidx.annotation.NonNull
+import android.util.Log
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
